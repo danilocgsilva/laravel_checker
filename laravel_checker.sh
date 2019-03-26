0b9fd8145eac3334266ec8ef01426b55b4c5f829
Danilo Silva <danilo.silva@doc88.com.br>
2019-03-26 16:38:00 -0300

    Checking permissions

--- laravel_checker.sh
+++ laravel_checker.sh
@@ -5,8 +5,51 @@ VERSION="0.0.0"
 
 ## Main function
 laravel_checker () {
+  verify_vendor
+  enter_vhost_user
+  verify_debug_log
+  verify_laravel_log
+  verify_cache
+}
+
+enter_vhost_user() {
+  read -p "Which is the name of the user host? " user_host
+  echo The host user is $user_host
+}
+
+verify_debug_log() {
+  logs_files=storage/logs/laravel.log
+
+  if [ ! -f $logs_files ]; then
+    echo The $logs_files does not exits.
+    echo Just execute: \`touch $logs_files\`, then set correct permission.
+    exit
+  fi
+}
+
+verify_laravel_log() {
+  logs_files=storage/logs/debug.log
+
+  if [ ! -f $logs_files ]; then
+    echo The $logs_files does not exits.
+    echo Just execute: \`touch $logs_files\`, then set correct permission.
+    exit
+  fi
+}
+
+verify_vendor() {
   if [ ! -d vendor ]; then
     echo You may install the project. Vendor folder is missing...
+    exit
+  fi
+}
+
+verify_cache() {
+  cache_folder=bootstrap/cache
+  cache_folder_owner=$(stat -c '%U' $cache_folder)
+
+  if [ $user_host != $cache_folder_owner ]; then
+    echo The cache file owner is not the user host ($user_host).
     exit
   fi
 }
